d8e7b14811f8fd84c677543b1558211d412880af
Francisco Alé Palacios <fraalepal@alum.us.es>
2025-01-11 20:59:30 +0100

    update metadata

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0ca06bc5d60ce241"; }
+/******/ 		__webpack_require__.h = function() { return "10632eb487e058a8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
