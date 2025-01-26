732258fec3aae886da209e1c442317d0ee29c910
Francisco Alé Palacios <fraalepal@alum.us.es>
2025-01-26 17:47:41 +0100

    navbar fix

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,9 @@
 "use client";
-
 import { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import questoria from '../../public/newblanco3.png'
-import { WaitlistModal } from './WaitlistModal'; // Asegúrate de importar correctamente el modal
+import questoria from '../../public/newblanco3.png';
+import { WaitlistModal } from './WaitlistModal';
 
 type NavbarMenuType = {
   name: string;
@@ -17,16 +16,13 @@ export function Navbar() {
   const menus: NavbarMenuType[] = [
     {
       name: 'Follow us on Patreon',
-      action: () => window.open('https://patreon.com/diceverse', '_blank'), // Abre Patreon en una nueva pestaña
+      action: () => window.open('https://patreon.com/diceverse', '_blank'),
     },
   ];
 
   return (
-    <div className="w-full px-4 fixed z-40 top-[18px] flex justify-center">
-      <header
-        className="w-full max-w-[500px] py-3 px-4 shadow-[0_-1px_0_1px_#33333350]
-        bg-black/30 backdrop-blur-[10px] rounded-2xl flex items-center justify-between"
-      >
+    <div className=" px-4 fixed z-40 top-[18px] flex justify-center">
+      <header className="w-full max-w-[500px] py-3 px-4 shadow-[0_-1px_0_1px_#33333350] bg-black/30 backdrop-blur-[10px] rounded-2xl flex items-center justify-between">
         <div className="w-[210px] h-[90px] relative shrink-0">
           <Link href="/" className="w-full h-full block">
             <Image
@@ -39,16 +35,13 @@ export function Navbar() {
             />
           </Link>
         </div>
-
-        <nav className="flex items-center ">
-          <ul className="flex items-center justify-center gap-6 ">
+        <nav className="flex items-center justify-end w-full">
+          <ul className="flex items-center justify-end gap-6">
             {menus.map((menu) => (
               <li key={menu.name}>
                 <button
                   onClick={menu.action}
-                  className="text-marfin text-m font-nunito-sans font-bold
-                    hover:text-white hover:underline focus:text-white focus:underline
-                    underline-offset-2 transition-all duration-200 whitespace-nowrap"
+                  className="text-marfin text-m font-nunito-sans font-bold hover:text-white hover:underline focus:text-white focus:underline underline-offset-2 transition-all duration-200 whitespace-nowrap"
                 >
                   {menu.name}
                 </button>
@@ -57,7 +50,6 @@ export function Navbar() {
           </ul>
         </nav>
       </header>
-
       {/* Modal de la lista de espera */}
       <WaitlistModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
     </div>
